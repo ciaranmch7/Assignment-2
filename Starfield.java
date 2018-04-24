618cb6ec29a7dc6a550136620a7e32c1f155aebb
Ciaran McHale <ciaranmch7@gmail.com>
2018-04-24 20:44:32 +0000

    close finished project

--- Starfield.java
+++ Starfield.java
@@ -9,8 +9,25 @@ public class Starfield extends PApplet{
 	Star stars[];
 	int STARS = 100;
 	
-	Enemey enemey;
-	 Ship ship;
+	
+	 PImage img;
+	 PImage eimg;
+	 
+	 
+	  
+	  int x;
+	  int y;
+	  int speed;
+	Ship ship;
+	
+	  
+	  public void Ship( int x, int y, int speed, PImage img ) {
+	    this.x = x;
+	    this.y = y;
+	    this.speed = speed;
+	    this.img = img;
+	  }
+	  
 	 
 	  
 	 
@@ -18,11 +35,10 @@ public class Starfield extends PApplet{
 		size(1000,1000);
 	}
 	public void setup() {
-		PImage eimg = loadImage("Feature.png");
-		enemey = new Enemey(width + eimg.width/2, height/2, 4, eimg);
+		eimg = loadImage("Feature.png");
+		 img = loadImage("player.jpg");
+		 ship = new Ship( 100, height/2, 3, img );
 		
-		PImage simg = loadImage("player.jpg");
-		ship = new Ship( 100, height/2, 3, simg);
 			frameRate( 25 );
 			  smooth();
 			 
@@ -46,14 +62,19 @@ public class Starfield extends PApplet{
 	      
 	      if ( keyPressed == true && key == CODED ) {
 	    	     if ( keyCode == UP ) {
-	    	       ship.up();
+	    	       up();
 	    	     } else if ( keyCode == DOWN ) {
-	    	      ship.down();
+	    	      down();
 	    	     }
 	    	  }  
-	     enemey.draw();
-	      ship.draw();
-	     
+	      pushMatrix();
+		    translate ( x, y );
+		    image( img, 0,300,width/8,height/8 );
+		    image(eimg,850,300,width/10,height/9);
+		    
+		    popMatrix();
+
+	    
 
 }
 
@@ -65,12 +86,18 @@ public class Starfield extends PApplet{
 	    this.x = x;
 	    this.y = y;
     this.z = z;
-	  }
 	}
+	}
+	 void up() {
+	    y -= speed;
+	    if ( y < img.height/2 ) { y = img.height/2; }
+	  }
+	  
+	   void down() {
+	    y += speed;
+	    if ( y > height - img.height/2 ) { y = height - img.height/2; }
+	  }
 	
 
 
 }
-
-
-
